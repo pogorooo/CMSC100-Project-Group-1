9b7cc73734e8e99b9a1ee3f7e9489f761a3359f5
camanzanido <camanzanido@up.edu.ph>
2024-05-25 01:12:27 +0800

    feat: created the Order Page

--- farm-to-table/src/pages/Home.js
+++ farm-to-table/src/pages/Home.js
@@ -11,8 +11,8 @@ export default function HomePage() {
         <video src={videoBg} autoPlay loop muted />
         {/* <img src={marketBg} alt= "Market background"/> */}
         <div className='content'>
-          <h1 id="title">FARMER-TO-TABLE</h1>
-          <p id="description">Welcome to Farmer-to-Table, where every purchase connects you directly to our country's farmers, bringing the freshest product straight to your doorstep.</p>
+          <h1 id="title">FARM-TO-TABLE</h1>
+          <p id="description">Welcome to Farm-to-Table, where every purchase connects you directly to our country's farmers, bringing the freshest product straight to your doorstep.</p>
           <Link to = {`/sign-in`}> <button id="getStarted">Get Started </button></Link>
         </div>
 
