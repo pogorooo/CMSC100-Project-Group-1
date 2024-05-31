a3f9bef553c9e7a3636aa12a180a79aadf68b639
Jean Nikolai Victorio <jlvictorio1@up.edu.ph>
2024-05-31 20:15:29 +0800

    feat: Added user schema and static methods for signing up and logging in

---
+++ farm-to-table/backend/controllers/userController.js
@@ -0,0 +1,41 @@
+const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
+
+// login a user
+const loginUser = async (req, res) => {
+  const {email, password} = req.body
+
+  try {
+    const user = await User.login(email, password)
+
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+}
+
+// signup a user
+const signupUser = async (req, res) => {
+  const { firstName, middleName, lastName, email, password } = req.body; // Include all required fields
+
+  try {
+    const user = await User.signup(firstName, middleName, lastName, email, password); // Pass all required fields to signup method
+
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+
+module.exports = { signupUser, loginUser }
