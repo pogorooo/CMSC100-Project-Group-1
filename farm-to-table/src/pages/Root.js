5ea0cf2d4a5ec086d4ac41a5d8cb28ad607c745e
Jem De Venecia <jmdevenecia@up.edu.ph>
2024-06-01 13:40:11 +0800

    ui: added logo to navbar

--- farm-to-table/src/pages/Root.js
+++ farm-to-table/src/pages/Root.js
@@ -1,43 +1,37 @@
 import { Outlet, Link } from 'react-router-dom';
+
 import{useState} from 'react';
-import './Root.css'
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import StorefrontIcon from '@mui/icons-material/Storefront';
-import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+
+import './Root.css' 
+import logo from '../assets/Farm-to-Table-Logo.png'
+
 export default function Root() {
 
   const [isCustomerView, setIsCustomerView] = useState(false);
   const [cartItems, setCartItems] = useState([]);
-  const[orders, setOrders] = useState([]);
 
     return (
       <>
         <nav>
           <ul>
 
-            <li className='title'><Link to={`/`} >Farm-to-table</Link></li>   
+            <li className='title'><Link to={`/`} ><img class="logo" src = {logo}/></Link></li>   
             <div className='rightSide'>  
               <li> <Link to ={`/about`}> About</Link></li>
               {/* to show the shop and shopping cart only when the user signed in (customer view) */}
               {isCustomerView && (
                 <li>
-                  <Link to={`/shop`}> 
-                  <StorefrontIcon/>
-                  </Link>
+                  <Link to={`/shop`}> Shop</Link>
                 </li>
               )}
               {isCustomerView && ( 
                 <li>
-                  <Link  to={`/cart`}>
-                    <ShoppingCartOutlinedIcon/>
-                  </Link>
+                  <Link  to={`/cart`}>Shopping Cart</Link>
                 </li>
               )}
               {isCustomerView && (
                 <li>
-                  <Link to={`/orders`}> 
-                  <ShoppingBagOutlinedIcon/>
-                  </Link>
+                  <Link to={`/orders`}> Orders</Link>
                 </li>
               )}
 
@@ -52,7 +46,7 @@ export default function Root() {
           </ul>
         </nav>
         
-        <Outlet context={{ setIsCustomerView, setCartItems, cartItems, setOrders, orders }}/>
+        <Outlet context={{ setIsCustomerView, setCartItems, cartItems }}/>
       </>
     )
   }
