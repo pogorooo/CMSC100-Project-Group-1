410a2b8aaa179a2d4c5eb289dbfce39a7f3913e1
Ashton Stephonie T. Matias <atmatias@up.edu.ph>
2024-06-01 22:22:03 +0800

    feat(signIn.js, signUp.js): made it a form that we can get data from onSubmit

--- farm-to-table/src/pages/SignUp.js
+++ farm-to-table/src/pages/SignUp.js
@@ -4,6 +4,17 @@ import './signInUpPages.css';
 import mp from '../assets/marketplace.jpg';
 
 export default function SignUp() {
+
+  //to get data
+  const handleSubmit=(event)=>{
+    event.preventDefault()
+    const data = new FormData(event.currentTarget);
+    const userData={
+      email: data.get("email"),
+      password: data.get("password")
+    }
+    console.log("userData, ", userData);
+  }
     return (
       <>
         <div class ="bg">
@@ -11,18 +22,19 @@ export default function SignUp() {
           <div class="main-box">
             <div class = "redirect">
               <p class="title">Already A Member?</p>
-              <img class = "mp" src={mp}/>
+              <img class = "mp" src={mp} alt="pic for signUp"/>
               <button className="page-button"><Link to= {`/sign-in`}>Sign in</Link></button>
             </div>
             <div class = "sign-in-up">
-              <form>
+
+              <form onSubmit={handleSubmit}>
                 <p class="title">Sign Up Here</p>
-                <input placeholder="Email"></input> <br></br>
-                <input placeholder="Password"></input> <br></br>
-                <input placeholder="First Name"></input> <br></br>
-                <input placeholder="Middle Name"></input> <br></br>
-                <input placeholder="Last Name"></input> <br></br>
-                <button className="page-button">Sign up</button>
+                <input required id="email" name="email" placeholder="Email"></input> <br></br>
+                <input required id="password" name="password" placeholder="Password"></input> <br></br>
+                <input required id="firstName" name="firstName" placeholder="First Name"></input> <br></br>
+                <input id="middleName" name="middleName" placeholder="Middle Name"></input> <br></br>
+                <input id="lastName" name="lastName" required placeholder="Last Name"></input> <br></br>
+                <button className="page-button" type="submit">Sign up</button>
               </form>
             </div>
           </div>
