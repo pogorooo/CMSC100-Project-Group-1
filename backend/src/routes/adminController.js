e0f9a0bac1b02f27abdee3d300178edc026d9f4a
Ashton Stephonie T. Matias <atmatias@up.edu.ph>
2024-06-01 13:09:25 +0800

    feat(backend/src/routes/adminController.js): created routes for admin order API

---
+++ backend/src/routes/adminController.js
@@ -0,0 +1,11 @@
+const express= require("express");
+const router=express.Router();
+
+const orderController=require("../controller/adminOrder.controller.js");
+const authenticate = require("../middleware/authenticate.js");
+
+router.get("/",authenticate, orderController.getAllOrders);
+router.put("/:orderId/confirmed",authenticate,orderController.confirmedOrders);
+router.put("/:orderId/cancel",authenticate,orderController.cancelledOrders);
+
+module.exports= router;
